67ce00bd322edd9cb54a7e5c5b001048165da3b8
Lucas Dias Developer <devweblucasdias@gmail.com>
2024-07-21 13:44:31 -0300

    atualização Carrousel => mobile e Desktop

--- scripts.js
+++ scripts.js
@@ -59,90 +59,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Carrossel
+
 document.addEventListener("DOMContentLoaded", function () {
-  const gallery = document.querySelector(".gallery");
-  const items = Array.from(document.querySelectorAll(".item"));
-  const totalItems = items.length;
-  let currentIndex = 0;
+  // Seleciona o contêiner .logos-slide
+  var original = document.querySelector(".scroll_container");
 
-  if (gallery && totalItems > 0) {
-    // Clonar a primeira e a última imagem para criar um efeito de rotação infinita
-    const firstItemClone = items[0].cloneNode(true);
-    const lastItemClone = items[totalItems - 1].cloneNode(true);
-    gallery.appendChild(firstItemClone);
-    gallery.insertBefore(lastItemClone, items[0]);
+  // Clona o contêiner e seu conteúdo
+  var copy = original.cloneNode();
 
-    const adjustedItems = Array.from(document.querySelectorAll(".item"));
-    const itemWidth = adjustedItems[0].offsetWidth;
-
-    // Ajustar a posição inicial do carrossel
-    gallery.style.transform = `translateX(-${itemWidth}px)`;
-
-    function updateCarousel() {
-      adjustedItems.forEach((item) => {
-        item.classList.remove("current-item");
-        item.style.opacity = "0.6";
-        item.style.transform = "scale(1)";
-      });
-
-      const centeredIndex = currentIndex + 1;
-      adjustedItems[centeredIndex].classList.add("current-item");
-      adjustedItems[centeredIndex].style.opacity = "1";
-      adjustedItems[centeredIndex].style.transform = "scale(1.4)";
-    }
-
-    function moveToIndex(index) {
-      gallery.style.transform = `translateX(-${(index + 1) * itemWidth}px)`;
-    }
-
-    function handleTransitionEnd() {
-      if (currentIndex === totalItems) {
-        currentIndex = 0;
-        gallery.style.transition = "none";
-        moveToIndex(currentIndex);
-        setTimeout(() => {
-          gallery.style.transition = "transform 0.5s ease";
-        }, 0);
-      } else if (currentIndex === -1) {
-        currentIndex = totalItems - 1;
-        gallery.style.transition = "none";
-        moveToIndex(currentIndex + 1);
-        setTimeout(() => {
-          gallery.style.transition = "transform 0.5s ease";
-        }, 0);
-      }
-    }
-
-    const prevButton = document.querySelector(".arrow-left");
-    const nextButton = document.querySelector(".arrow-right");
-
-    if (prevButton) {
-      prevButton.addEventListener("click", () => {
-        currentIndex--;
-        if (currentIndex < 0) currentIndex = totalItems - 1;
-        moveToIndex(currentIndex + 1);
-        updateCarousel();
-        handleTransitionEnd();
-      });
-    }
-
-    if (nextButton) {
-      nextButton.addEventListener("click", () => {
-        currentIndex++;
-        if (currentIndex >= totalItems) currentIndex = 0;
-        moveToIndex(currentIndex + 1);
-        updateCarousel();
-        handleTransitionEnd();
-      });
-    }
-
-    gallery.addEventListener("transitionend", handleTransitionEnd);
-
-    // Inicializar exibição
-    updateCarousel();
-  }
+  // Adiciona o contêiner clonado ao contêiner pai .logos
+  original.parentElement.appendChild(copy);
 });
 
+//expand modal maps
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".toggle-btn");
   const mapContainer = document.getElementById("map-container");
