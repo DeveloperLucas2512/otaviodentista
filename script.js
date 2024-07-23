9485f6a84b7a0a168584115e5d92f41f28f4a187
Lucas Dias Developer <devweblucasdias@gmail.com>
2024-07-23 20:17:05 -0300

    botoes verde e modal com descricao

--- script.js
+++ script.js
@@ -31,49 +31,59 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Carrossel
   document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.querySelector(".toggle-btn");
-    const mapContainer = document.getElementById("map-container");
-    const image = document.querySelector(".map-container .image img");
+    // Adiciona funcionalidade ao botão Saiba Mais
+    const saibaMaisBtns = document.querySelectorAll(".saibaMaisBtn");
 
-    if (toggleBtn && mapContainer && image) {
-      toggleBtn.addEventListener("click", function () {
-        if (mapContainer.classList.contains("collapsed")) {
-          mapContainer.classList.remove("collapsed");
-          mapContainer.classList.add("expanded");
-          toggleBtn.classList.remove("fa-arrow-down");
-          toggleBtn.classList.add("fa-arrow-up");
+    saibaMaisBtns.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        const contentId = btn.getAttribute("data-content");
+        const expandableContent = document.getElementById(
+          `conteudo-${contentId}`
+        );
 
-          // Adiciona a animação à imagem quando expandida
-          image.classList.add("animate");
+        // Toggle visibility
+        if (expandableContent.classList.contains("show")) {
+          expandableContent.classList.remove("show");
         } else {
-          mapContainer.classList.remove("expanded");
-          mapContainer.classList.add("collapsed");
-          toggleBtn.classList.remove("fa-arrow-up");
-          toggleBtn.classList.add("fa-arrow-down");
-
-          // Remove a animação da imagem quando recolhida
-          image.classList.remove("animate");
+          // Hide all other contents
+          document
+            .querySelectorAll(".expandable-content")
+            .forEach((content) => content.classList.remove("show"));
+          expandableContent.classList.add("show");
         }
       });
-    }
+    });
   });
 
   var contentData = {
     1: `
-      <h2>Ortognática</h2>
-      <p>Conteúdo detalhado sobre Ortognática.</p>
+     <h3>Ortognática</h3>
+      <p>A Ortognática é uma especialidade da cirurgia bucomaxilofacial que se concentra no tratamento de irregularidades nos ossos da face e maxilares.</p>
+      <p>O objetivo é corrigir problemas como o desalinhamento dos dentes e mandíbulas, melhorando a função mastigatória, a estética facial e a saúde geral.</p>
+      <p>Este tipo de cirurgia é indicado para pessoas com discrepâncias significativas entre os maxilares superior e inferior, que podem causar dificuldades na fala, mastigação ou problemas respiratórios. A Ortognática pode também aliviar problemas de mordida e melhorar a aparência facial, proporcionando um sorriso mais harmonioso e equilibrado.</p>
+
+
     `,
     2: `
-      <h2>Implante Dentário</h2>
-      <p>Conteúdo detalhado sobre Implante Dentário.</p>
+     <h3>Implante Dentário</h3>
+      <p>O implante dentário é uma solução moderna e eficaz para substituir dentes perdidos ou danificados. Consiste em um pequeno parafuso de titânio que é inserido cirurgicamente no osso maxilar, servindo como uma raiz artificial para suportar uma coroa dentária.</p>
+      <p>Esse procedimento visa restaurar a função mastigatória e melhorar a estética do sorriso, proporcionando uma aparência e sensação natural. Os implantes dentários são uma opção durável e de longo prazo, que também ajuda a preservar a estrutura óssea e a saúde bucal geral.</p>
+      <p>Ideal para pessoas que perderam dentes devido a cáries, traumas ou doenças periodontais, o implante dentário oferece uma alternativa sólida e segura às próteses removíveis e pontes dentárias, promovendo conforto e estabilidade.</p>
+
     `,
     3: `
-      <h2>Excesso Vertical de Maxila</h2>
-      <p>Conteúdo detalhado sobre Excesso Vertical de Maxila.</p>
+     <h2>Excesso Vertical de Maxila</h2>
+      < problema pode resultar em um perfil facial desproporcional, onde a parte superior da face parece exageradamente alongada em comparap>O excesso vertical de maxila é uma condição onde há um aumento anormal no comprimento da maxila, a parte superior do maxilar. Esteção com a parte inferior.</p>
+      <p>Este distúrbio pode causar dificuldades funcionais, como problemas de mordida e mastigação, além de afetar a estética facial. O tratamento para o excesso vertical de maxila geralmente envolve cirurgia ortognática para reduzir a altura da maxila e melhorar o equilíbrio facial.</p>
+      <p>O objetivo da cirurgia é restaurar a harmonia facial, melhorar a função mastigatória e promover um sorriso mais estético e confortável.</p>
+
     `,
     4: `
-      <h2>Sorriso Gengival</h2>
-      <p>Conteúdo detalhado sobre Sorriso Gengival.</p>
+     <h2>Sorriso Gengival</h2>
+      <p>O sorriso gengival ocorre quando uma quantidade excessiva de gengiva é visível ao sorrir, o que pode causar desconforto estético para muitas pessoas. Esta condição pode ser causada por fatores genéticos, hiperatividade dos músculos que elevam o lábio superior, ou por um crescimento desigual dos dentes.</p>
+      <p>Os tratamentos para o sorriso gengival variam de acordo com a causa. Pode incluir procedimentos estéticos, como a correção da linha gengival através de cirurgia plástica gengival, ou tratamentos ortodônticos para ajustar a posição dos dentes. O objetivo é alcançar um sorriso mais equilibrado, onde a quantidade de gengiva visível é reduzida, melhorando a aparência e a confiança do paciente.</p>
+      <p>Em alguns casos, a correção pode envolver uma combinação de abordagens para garantir resultados duradouros e satisfatórios.</p>
+
     `,
   };
 
